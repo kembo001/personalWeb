5637a27e29a59239515504461a0ba22cdfcfeef0
Brandon K <brandonk@Brandons-MacBook-Pro.local>
2022-04-23 17:36:32 -0500

    added nav bar

---
+++ personal-web/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCnDjiztkm0SQYjE5WtZ1j5rCM0GdlJ418",
+  authDomain: "personal-website-dbf2e.firebaseapp.com",
+  projectId: "personal-website-dbf2e",
+  storageBucket: "personal-website-dbf2e.appspot.com",
+  messagingSenderId: "78197352696",
+  appId: "1:78197352696:web:39bdfd65d2bd016c39b30d",
+  measurementId: "G-8PZC7MTV5Q"
+};
