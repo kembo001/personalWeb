d8e1e27e39fcfedd2b5ec4c3fe1fbc8bdf82d50b
kembo001 <47574348+kembo001@users.noreply.github.com>
2022-04-25 14:01:01 -0500

    added components

--- personal-web/src/App.js
+++ personal-web/src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
-import Header from './Header.js'
-import './image/background.png'
+import Header from './components/Header'
+import Nav from './components/Nav'
+import Skills from './components/Skills'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
-      <img src='/image/background.png'/>
       <Header />
+      <Nav />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
