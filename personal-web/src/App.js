c1c0787bc46a6c2b779d3782a7b2f8e5c6f7375a
Brandon K <brandonk@Brandons-MacBook-Pro.local>
2022-04-24 22:09:19 -0500

    added images

--- personal-web/src/App.js
+++ personal-web/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Header from './Header.js'
+import './image/background.png'
 
 function App() {
   return (
     <div className="App">
+      <img src='/image/background.png'/>
       <Header />
     </div>
   );
