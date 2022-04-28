8184ed514f98c4c555a358554698fea647e2e87e
kembo001 <47574348+kembo001@users.noreply.github.com>
2022-04-27 23:39:14 -0500

    added framer animation for header

--- personal-web/src/components/Header.js
+++ personal-web/src/components/Header.js
@@ -3,20 +3,38 @@ import './Header.css'
 import Resume from './Resume'
 import Me from '../image/newMe.jpg'
 import Socials from './Socials'
+import styled from 'styled-components'
+import {motion} from 'framer-motion'
+
+const fadeLeft ={
+  hidden: {opacity: 0, y:100},
+  visible: {opacity:1, y:0}
+}
+
 
 function Header() {
   return (
     <header id='home'>
-      <div className= 'container headerContainer'>
+      <motion.div
+          initial={{opacity: 0}}
+          animate={{opacity: 1}}
+          transition={{duration: 1}} className= 'container headerContainer'>
         <h5 className='text-light'>Hello, My name is</h5>
-        <h1>Brandon Kemboi</h1>
+        <h1
+        >Brandon Kemboi</h1>
         <h5 className='text-light'>Web Developer & Problem Solver</h5>
         <Resume />
         <Socials />
         <div className='me'>
-          <img src={Me} alt=''/>
+          <motion.img
+          variants={fadeLeft}
+          initial='hidden'
+          animate='visible'
+          transition={{duration: 1}}
+
+          src={Me} alt=''/>
         </div>
-      </div>
+      </motion.div>
     </header>
   )
 }
